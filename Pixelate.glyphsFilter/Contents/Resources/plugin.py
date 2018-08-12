004e3d392b11bf4042869ee0143cf6723e56f1f3
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2018-08-12 15:17:56 +0200

    Added license, code

---
+++ Pixelate.glyphsFilter/Contents/Resources/plugin.py
@@ -0,0 +1,173 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Filter with dialog Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Filter%20with%20Dialog
+#
+#	For help on the use of Interface Builder:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates
+#
+#
+###########################################################################################################
+
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+class Pixelate(FilterWithDialog):
+	
+	# Definitions of IBOutlets
+	
+	# The NSView object from the User Interface. Keep this here!
+	dialog = objc.IBOutlet()
+	
+	# Text field in dialog
+	pixelComponentNameField = objc.IBOutlet()
+	pixelRasterWidthField = objc.IBOutlet()
+	resetWidthsField = objc.IBOutlet()
+	
+	
+	def settings(self):
+		self.menuName = Glyphs.localize({
+			'en': u'Pixelate',
+			'de': u'Verpixeln'
+		})
+		
+		# Word on Run Button (default: Apply)
+		self.actionButtonLabel = Glyphs.localize({
+			'en': u'Pixelate',
+			'de': u'Verpixeln'
+		})
+		
+		# Load dialog from .nib (without .extension)
+		self.loadNib('IBdialog', __file__)
+	
+	# On dialog show
+	def start(self):
+		
+		# Set default value
+		Glyphs.registerDefault("com.mekkablue.Pixelate.pixelComponentName", "pixel")
+		Glyphs.registerDefault("com.mekkablue.Pixelate.pixelRasterWidth", "50")
+		Glyphs.registerDefault("com.mekkablue.Pixelate.resetWidths", True)
+		
+		# Set value of text field
+		self.pixelComponentNameField.setStringValue_( Glyphs.defaults["com.mekkablue.Pixelate.pixelComponentName"] )
+		self.pixelRasterWidthField.setStringValue_( Glyphs.defaults["com.mekkablue.Pixelate.pixelRasterWidth"] )
+		self.resetWidthsField.setState_( int(bool(Glyphs.defaults["com.mekkablue.Pixelate.resetWidths"])) )
+		
+		# Set focus to text field
+		self.pixelComponentNameField.becomeFirstResponder()
+		
+	# Actions triggered by UI:
+	@objc.IBAction
+	def setPixelComponentName_( self, sender ):
+		Glyphs.defaults['com.mekkablue.Pixelate.pixelComponentName'] = sender.stringValue()
+		self.update()
+		
+	@objc.IBAction
+	def setPixelRasterWidth_( self, sender ):
+		Glyphs.defaults['com.mekkablue.Pixelate.pixelRasterWidth'] = sender.intValue()
+		self.update()
+		
+	@objc.IBAction
+	def setResetWidths_( self, sender ):
+		Glyphs.defaults['com.mekkablue.Pixelate.resetWidths'] = sender.state()
+		self.update()
+	
+	# Actual filter
+	def filter(self, thisLayer, inEditView, customParameters):
+		
+		# Called on font export, get values from customParameters
+		if customParameters.has_key('snapwidth'):
+			widthsShouldBeReset = customParameters['snapwidth']
+		if customParameters.has_key('grid'):
+			pixelRasterWidth = customParameters['grid']
+		if customParameters.has_key('component'):
+			pixelNameEntered = customParameters['component']
+		
+		# Called through UI, use stored values
+		else:
+			widthsShouldBeReset = bool(Glyphs.defaults['com.mekkablue.Pixelate.resetWidths'])
+			pixelRasterWidth = int(Glyphs.defaults['com.mekkablue.Pixelate.pixelRasterWidth'])
+			pixelNameEntered = str(Glyphs.defaults['com.mekkablue.Pixelate.pixelComponentName'])
+		
+		try:
+			thisGlyph = thisLayer.parent
+			thisFont = thisGlyph.parent
+			pixelRasterWidth = max(5,abs(pixelRasterWidth)) # safety precaution
+			
+			if thisFont and thisGlyph.name != pixelNameEntered:
+				pixel = thisFont.glyphs[ pixelNameEntered ]
+				
+				# remove existing pixel components to avoid endless iteration:
+				for i in range(len(thisLayer.components))[::-1]:
+					if thisLayer.components[i].componentName == pixelNameEntered:
+						del thisLayer.components[i]
+				
+				if len(thisLayer.paths) > 0 or len(thisLayer.components) > 0:
+					# get all possible pixel positions within layer bounds:
+					thisLayerBezierPath = thisLayer.copyDecomposedLayer().bezierPath # necessary for containsPoint_() function
+					layerBounds = thisLayer.bounds
+					xStart = int(round( layerBounds.origin.x / pixelRasterWidth ))
+					yStart = int(round( layerBounds.origin.y / pixelRasterWidth ))
+					xIterations = int(round( layerBounds.size.width / pixelRasterWidth ))
+					yIterations = int(round( layerBounds.size.height / pixelRasterWidth ))
+					pixelCount = 0
+
+					if inEditView:
+						# move current layer to background and clean foreground:
+						thisLayer.background.clear()
+						thisLayer.swapForegroundWithBackground()
+					else:
+						thisLayer.clear()
+					
+					# snap width to pixel grid:
+					if widthsShouldBeReset:
+						originalWidth = thisLayer.width
+						pixelatedWidth = round( originalWidth / pixelRasterWidth ) * pixelRasterWidth
+						thisLayer.setWidth_( pixelatedWidth )
+				
+					# add pixels:
+					for x in range(xStart, xStart + xIterations):
+						for y in range( yStart, yStart + yIterations):
+							# if the pixel center is black, insert a pixel component here:
+							pixelCenter = NSPoint( (x+0.5) * pixelRasterWidth, (y+0.5) * pixelRasterWidth )
+							if thisLayerBezierPath.containsPoint_( pixelCenter ):
+								pixelCount += 1
+								pixelComponent = GSComponent( pixel, NSPoint( x * pixelRasterWidth, y * pixelRasterWidth ) )
+								pixelComponent.alignment = -1 # prevent automatic alignment
+								thisLayer.addComponent_( pixelComponent )
+					
+					if not inEditView:
+						thisLayer.decomposeComponents()
+						thisLayer.removeOverlap()
+
+					# print "%s: added %i pixels to layer '%s'%s." % ( thisGlyph.name, pixelCount, thisLayer.name )
+			
+		except Exception, e:
+			import traceback
+			t = str(traceback.format_exc())
+			errorMsg = "Pixelate Error: %s\n%s" % (e, t)
+			self.logToConsole( errorMsg )
+
+			# brings macro window to front and reports error:
+			if inEditView:
+				Glyphs.showMacroWindow()
+				print errorMsg
+			
+	
+	def generateCustomParameter( self ):
+		return "%s; grid:%s; component:%s; snapwidth:%s" % (
+			self.__class__.__name__, 
+			Glyphs.defaults["com.mekkablue.Pixelate.pixelRasterWidth"],
+			Glyphs.defaults["com.mekkablue.Pixelate.pixelComponentName"],
+			Glyphs.defaults["com.mekkablue.Pixelate.resetWidths"],
+			)
+	
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Pixelate
-Glyphs.app plug-in for rasterising shapes with a user-defined pixel glyph
+
+[Glyphs.app](https://glyphsapp.com/) filter plug-in for rasterising shapes with a user-defined pixel glyph.
+
+After installation, it will add the menu item *Filter > Pixelate*. You can set a keyboard shortcut in System Preferences.
+
+![Pixelate](Pixelate.png)
+
+### Installation
+
+1. In Glyphs, go to *Window > Plugin Manager.*
+2. Click the *Install* button next to the Pixelate entry.
+3. Restart Glyphs.
+
+### Usage Instructions
+
+1. Open a glyph in Edit View, or select any number of glyphs in Font or Edit View.
+2. Use *Filter > Pixelate* to insert pixels.
+
+* **Pixel glyph:** The name of the glyph you want to insert as pixel components.
+* **Pixel grid step:** The step width of the pixel raster, in units. This is the distance between component origins.
+* **Snap widths to pixel grid:** Should the glyph width be rounded to the pixel grid as well?
+
+Alternatively, you can also use it as a custom parameter. Click the gear button in the lower left corner and choose *Copy Custom Parameter* from the pop-up menu that appears, and paste it in *File > Font Info > INstances > Custom Parameters,* e.g.:
+
+	Property: Filter
+	Value: Pixelate; grid:70; component:_pixel; snapwidth:1
+
+At the end of the parameter value, you can hang `;exclude:` or `;include:`, followed by a comma-separated list of glyph names. This will apply the filter only to the included glyphs, or the glyphs not excluded, respectively.
+
+### Requirements
+
+The plugin needs Glyphs 2.5.2 or higher, running on OS X 10.9 or later. I can only test it in current OS versions, and cannot guarantee its functionality in previous app and macOS versions.
+
+### License
+
+Copyright 2018 Rainer Erich Scheichelbauer (@mekkablue).
+Based on his script of the same name and sample code by Georg Seifert (@schriftgestalt) and Jan Gerner (@yanone).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
+
